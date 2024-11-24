c9b99d2043e7322cc261c858f697b21f00958f6b
Davis-don <davismugoikou@gmail.com>
2024-11-24 23:34:16 +0300

    testimonials

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from './Components/Header/Header'
 import Hero from './Components/Hero/Hero'
 import Stories from './Components/stories/stories'
+import Testimonial from './Components/Testimonials/testimonials'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
 <Header/>
 <Hero/>
 <Stories/>
+<Testimonial/>
     </div>
   )
 }
