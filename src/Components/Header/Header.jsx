4e43401a9c69338c34cc61bb75522a1ee5fcffad
Davis-don <davismugoikou@gmail.com>
2024-11-24 17:54:00 +0300

    first commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './header.css'
+function Header() {
+  return (
+    <div className='overall-header-section'>
+        <h1>Header</h1>
+        </div>
+  )
+}
+
+export default Header
