7f9348652da99f6f26bde243eb1df52184181f0d
Gidayi Millyannah <millyannahi@gmail.com>
2024-11-25 12:11:18 +0300

    footer and categories

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import Header from "./Components/Header/Header";
 import Hero from "./Components/Hero/Hero";
 import Stories from "./Components/stories/stories";
 import Testimonial from "./Components/Testimonials/testimonials";
+import Footer from "./Components/Footer/Footer";
+import Categories from "./Components/Categories/categories";
 
 function App() {
   return (
     <div className="app">
       <Header />
       <Hero />
+      <Categories/>
       <Stories />
       <Testimonial />
+      <Footer/>
     </div>
   );
 }
