77f23438c4ef9aef2e8546c66080567c3351ebff
Davis-don <davismugoikou@gmail.com>
2024-11-25 11:44:27 +0300

    special deshes added

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Header from './Components/Header/Header'
 import Hero from './Components/Hero/Hero'
 import Stories from './Components/stories/stories'
 import Testimonial from './Components/Testimonials/testimonials'
+import SpecialDishes from "./Components/SpecialDishes/specialDishes";
 
 function App() {
   return (
     <div className='app'>
 <Header/>
 <Hero/>
+<SpecialDishes/>
 <Stories/>
 <Testimonial/>
     </div>
